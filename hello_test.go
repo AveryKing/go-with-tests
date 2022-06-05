051f1c33de48d309ffa9535c8e6a17bb0a59ae51
Avery King <kavery086@gmail.com>
2022-06-05 04:25:45 -0500

    handling added for no name hello

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Avery")
-	want := "Hello, Avery"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Avery")
+		want := "Hello, Avery"
 
-	if got != want {
-		t.Errorf("got %q  want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q  want %q", got, want)
+		}
+	})
+	t.Run("hello to world if no name", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
