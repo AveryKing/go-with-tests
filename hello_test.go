8a8467e3acf6b86617ebee94b39aaf0f4711f63b
Avery King <kavery086@gmail.com>
2022-06-05 04:40:33 -0500

    assert refactored into function

--- hello_test.go
+++ hello_test.go
@@ -3,20 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Avery")
-		want := "Hello, Avery"
-
-		if got != want {
-			t.Errorf("got %q  want %q", got, want)
-		}
-	})
-	t.Run("hello to world if no name", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, world"
-
+	assetCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Avery")
+		want := "Hello, Avery"
+		assetCorrectMessage(t, got, want)
+	})
+
+	t.Run("hello to world if no name", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assetCorrectMessage(t, got, want)
 	})
 }
