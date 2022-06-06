568603e09ad1c2fd64e3ee13b383da33c868a750
Avery King <kavery086@gmail.com>
2022-06-05 23:15:37 -0500

    in french test passing

--- hello_test.go
+++ hello_test.go
@@ -27,4 +27,10 @@ func TestHello(t *testing.T) {
 		want := "Hola, Avery"
 		assetCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Avery", "FR")
+		want := "Bonjour, Avery"
+		assetCorrectMessage(t, got, want)
+	})
 }
