e478210b705009c7e577c91dd5807d62fe3d1baa
Avery King <kavery086@gmail.com>
2022-06-05 23:12:00 -0500

    .

--- hello.go
+++ hello.go
@@ -2,20 +2,24 @@ package main
 
 import "fmt"
 
+const spanish = "ES"
+const french = "FR"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, lang string) string {
-	var prefixUsed string = englishHelloPrefix
-
-	if lang == "ES" {
-		prefixUsed = spanishHelloPrefix
+	prefix := englishHelloPrefix
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
 	}
-
 	if name == "" {
 		name = "world"
 	}
-	return prefixUsed + name
+	return prefix + name
 }
 
 func main() {
