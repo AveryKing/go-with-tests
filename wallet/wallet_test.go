f01286ce7466f6ad12ef462dbef69bbba21fe755
Avery King <kavery086@gmail.com>
2022-06-09 20:27:26 -0500

    Bitcoin type added

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -7,10 +7,10 @@ import (
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 	got := wallet.Balance()
 	fmt.Printf("test wallet balance is %v", &wallet.balance)
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
