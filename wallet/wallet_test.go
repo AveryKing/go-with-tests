cfa85039a48e1a2d28dee141cad3223174d7d974
Avery King <kavery086@gmail.com>
2022-06-09 20:36:40 -0500

    withdraw method added

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -5,12 +5,26 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(25)
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(15)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
