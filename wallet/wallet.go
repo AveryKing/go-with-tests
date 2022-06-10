f611b3baf487f1c750ecf742f59142c746462841
Avery King <kavery086@gmail.com>
2022-06-09 20:33:45 -0500

    Stringer implemented for Bitcoin type

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -8,11 +8,18 @@ type Wallet struct {
 	balance Bitcoin
 }
 
+type Stringer interface {
+	String() string
+}
+
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("Deposit wallet balance is \n %v", &w.balance)
 	w.balance += amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,10 +8,9 @@ func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 	wallet.Deposit(Bitcoin(10))
 	got := wallet.Balance()
-	fmt.Printf("test wallet balance is %v", &wallet.balance)
 	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
